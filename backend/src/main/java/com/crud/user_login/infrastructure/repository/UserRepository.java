d0a026c0421470153f514dadaf3da346a393466c
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-02 16:37:04 -0300

    Update UserRepository.java

--- backend/src/main/java/com/crud/user_login/infrastructure/repository/UserRepository.java
+++ backend/src/main/java/com/crud/user_login/infrastructure/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.crud.user_login.infrastructure.repository;
 
-import com.kauan.user_login.infrastructure.entitys.Usuario;
+import com.crud.user_login.infrastructure.entitys.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
